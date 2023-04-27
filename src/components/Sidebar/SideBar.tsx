bdc363c8e3a1b46768218cc67ae69c05003fc80b
Hristijan Petkovski <hristijan.petkovski@keitaro.com>
2023-04-27 13:48:09 +0200

    Create sidebar header

--- src/components/Sidebar/SideBar.tsx
+++ src/components/Sidebar/SideBar.tsx
@@ -1,10 +1,12 @@
-import { SideBarHeder } from '../SideBarHeder/SideBarHeder';
 import './SideBar.scss';
 
 export const SideBar = () => {
   return (
     <div className="side-bar-layout">
-      <SideBarHeder />
+      <div className="side-bar-header text-center sticky-top">
+        <img className="side-bar-logo" src={'keitaroLogo'} />
+        <p className="side-bar-title">Blog Image Generator</p>
+      </div>
     </div>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export { SideBar } from './Sidebar/SideBar';
 export { CanvasWrapper } from './CanvasWrapper/CanvasWrapper';
-export { SideBarHeder } from './SideBarHeder/SideBarHeder';
