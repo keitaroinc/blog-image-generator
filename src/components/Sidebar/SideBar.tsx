1a96fc26957ed45596c8666d722bc19fa4f0cc3e
Nikola Aksentijev <nikola.aksentijev@keitaro.com>
2023-05-02 11:25:12 +0200

    added basic unit test

--- src/components/Sidebar/SideBar.tsx
+++ src/components/Sidebar/SideBar.tsx
@@ -3,7 +3,7 @@ import { CanvasHeadline } from '../CanvasHeadline/CanvasHeadline';
 
 export const SideBar = () => {
   return (
-    <div className="side-bar-layout p-0">
+    <div data-testid="Sidebar" className="p-0">
       <div className="bg-gray-light sticky-top d-flex justify-content-center flex-column align-items-center py-5">
         <img className="mb-2" src={'keitaroLogo'} alt="Keitaro Logo" />
         <p className="mb-0 bg-white py-1 px-2 fs-6 fw-bold">Blog Image Generator</p>
