dc6c7d56ffd27bffefb6666fb2bbdcd2ad4658ec
Nikola Aksentijev <nikola.aksentijev@keitaro.com>
2023-05-02 11:25:12 +0200

    added basic unit test

---
+++ src/components/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import { App } from '../../App';
+
+describe('SideBar component', () => {
+  it('renders a container with test id "Sidebar"', () => {
+    render(<App />);
+    expect(screen.getByTestId("Sidebar")).toBeInTheDocument();
+  });
+});
