4b461c56e89be17263c51cbd5b5dff564688a768
Goce Mitevski <goce.mitevski@keitaro.com>
2023-05-03 09:37:46 +0200

    Remove wrapper and update SideBar inlcude

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import './assets/scss/style.scss';
-import { CanvasWrapper, SideBar } from './components';
+import React from "react";
+import "./assets/scss/style.scss";
+import { CanvasWrapper, SideBar } from "./components";
 
 export const App = () => {
   return (
-    <div className="row wrap g-0 vh-100">
-      <div className="col-md-3 col-sm-4 d-flex flex-column flex-grow-1 overflow-auto bg-gray-light p-0">
-        <SideBar />
-      </div>
+    <div className="row g-0 vh-100 overflow-hidden">
+      <SideBar className="col-md-3 col-sm-4 d-flex flex-column bg-gray-light sidebar h-100 overflow-auto" />
       <div className="col-md-9 col-sm-8">
         <CanvasWrapper />
       </div>
