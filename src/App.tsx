2445552fecf36e15cf152a86832009c3c0997b81
Nikola Aksentijev <nikola.aksentijev@keitaro.com>
2023-05-03 11:16:13 +0200

    Clearing unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './assets/scss/style.scss';
 import { CanvasWrapper, SideBar } from './components';
-import { CanvasBackgroundContextProvider } from './contexts';
 
 export const App = () => {
   return (
