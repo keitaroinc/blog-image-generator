26c08f287f3b59a419e03e1e6c478bbf55e9bad3
Hristijan Petkovski <hristijan.petkovski@keitaro.com>
2023-05-03 10:31:49 +0200

    Implement and resolve issue #16

--- src/components/Sidebar/SideBar.tsx
+++ src/components/Sidebar/SideBar.tsx
@@ -1,14 +1,14 @@
-import { CanvasBackgroundContextProvider, CanvasHeadlineContextProvider } from '../../contexts';
+import { CanvasBackgroundContextProvider, CanvasHeadlineContextProvider, CanvasGradientContextProvider } from '../../contexts';
 import { CanvasBackground } from '../CanvasBackground/CanvasBackground';
 import { CanvasHeadline } from '../CanvasHeadline/CanvasHeadline';
 import { CanvasGradient } from '../CanvasGradient/CanvasGradient';
-import { CanvasGradientContextProvider } from '../../contexts/GradientContext';
+import keitaroLogo from '../../assets/svg/keitaro-logo-full-color.svg';
 
 export const SideBar: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div data-testid="Sidebar" className={className}>
       <div className="bg-gray-light sticky-top d-flex justify-content-center flex-column align-items-center py-5">
-        <img className="mb-2" src={"keitaroLogo"} alt="Keitaro Logo" />
+        <img className="mb-2" width={"200px"} src={keitaroLogo} alt="Keitaro Logo" />
         <p className="mb-0 bg-white py-1 px-2 fs-6 fw-bold">
           Blog Image Generator
         </p>
