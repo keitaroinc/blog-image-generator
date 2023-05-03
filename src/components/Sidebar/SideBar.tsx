2649b27436a63ee11884b9ffe5c6fbe2da162063
Goce Mitevski <goce.mitevski@keitaro.com>
2023-05-03 09:37:27 +0200

    Refactor SideBar component

--- src/components/Sidebar/SideBar.tsx
+++ src/components/Sidebar/SideBar.tsx
@@ -4,13 +4,14 @@ import { CanvasHeadline } from '../CanvasHeadline/CanvasHeadline';
 import { CanvasGradient } from '../CanvasGradient/CanvasGradient';
 import { CanvasGradientContextProvider } from '../../contexts/GradientContext';
 
-
-export const SideBar = () => {
+export const SideBar: React.FC<{ className?: string }> = ({ className }) => {
   return (
-    <div data-testid="Sidebar" className="p-0">
+    <div data-testid="Sidebar" className={className}>
       <div className="bg-gray-light sticky-top d-flex justify-content-center flex-column align-items-center py-5">
-        <img className="mb-2" src={'keitaroLogo'} alt="Keitaro Logo" />
-        <p className="mb-0 bg-white py-1 px-2 fs-6 fw-bold">Blog Image Generator</p>
+        <img className="mb-2" src={"keitaroLogo"} alt="Keitaro Logo" />
+        <p className="mb-0 bg-white py-1 px-2 fs-6 fw-bold">
+          Blog Image Generator
+        </p>
       </div>
       <CanvasHeadlineContextProvider>
         <CanvasHeadline />
