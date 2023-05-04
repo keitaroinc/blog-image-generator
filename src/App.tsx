24438a3424767c66712d4d0a8ee2cae84d4ac3de
Goce Mitevski <goce.mitevski@keitaro.com>
2023-05-04 10:34:52 +0200

    Update CanvasWrapper include

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ export const App = () => {
   return (
     <div className="row g-0 vh-100 overflow-hidden">
       <SideBar className="col-md-3 col-sm-4 d-flex flex-column bg-gray-light sidebar h-100 overflow-auto" />
-      <div className="col-md-9 col-sm-8">
-        <CanvasWrapper />
-      </div>
+      <CanvasWrapper className="col-md-9 col-sm-8 d-flex flex-column justify-content-center align-items-center canvas-wrapper" />
     </div>
   );
 };
