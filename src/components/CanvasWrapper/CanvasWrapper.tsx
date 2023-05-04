4a19b967b83d7485f484a39c6d74312ab5df5830
Goce Mitevski <goce.mitevski@keitaro.com>
2023-05-04 10:35:46 +0200

    Refactor CanvasWrapper

--- src/components/CanvasWrapper/CanvasWrapper.tsx
+++ src/components/CanvasWrapper/CanvasWrapper.tsx
@@ -1,5 +1,29 @@
-import * as React from 'react';
+import * as React from "react";
+import "./CanvasWrapper.scss";
 
-export const CanvasWrapper = () => {
-  return <div className='sticky-top vh-100'>Canvas Wrapper</div>;
+export const CanvasWrapper: React.FC<{ className?: string }> = ({
+  className,
+}) => {
+  return (
+    <div className={className}>
+      <div className="canvas grid flex-grow-1">
+        <h1 className="title w-75 ps-5">
+          Headline text goes here and it can get pretty long
+        </h1>
+        <div className="icon">
+          <img
+            className="w-25 h-25 justify-self-end"
+            src="/image-generator/static/media/keitaro-logo-full-color.44775c6e1a17ea7494df1b4c93c5dd09.svg"
+            alt="Keitaro logo"
+          />
+        </div>
+        <img
+          className="align-self-end logo"
+          src="/image-generator/static/media/keitaro-logo-full-color.44775c6e1a17ea7494df1b4c93c5dd09.svg"
+          alt="Keitaro logo"
+        />
+      </div>
+      <button className="btn btn-success">Download</button>
+    </div>
+  );
 };
