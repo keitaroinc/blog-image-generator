c6215452d61ae51c78834dbaba9186fa12e728f1
Goce Mitevski <goce.mitevski@keitaro.com>
2023-05-05 11:53:53 +0200

    Add CSS classes

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -5,7 +5,7 @@ import keitaroLogo from '../../assets/svg/keitaro-logo-full-color.svg';
 export const SideBar: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div data-testid="Sidebar" className={className}>
-      <div className="bg-gray-light sticky-top d-flex justify-content-center flex-column align-items-center py-5">
+      <div className="bg-gray-light sticky-top shadow shadow-lg d-flex justify-content-center flex-column align-items-center py-5 mb-2">
         <img className="sidebar-logo mb-2" src={keitaroLogo} alt="Keitaro Logo" />
         <p className="mb-0 bg-white py-1 px-2 fs-6 fw-bold">
           Blog Image Generator
