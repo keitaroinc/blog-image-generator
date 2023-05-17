a5f785a61231c8472f619b8e115004fb4ded5b83
Goce Mitevski <goce.mitevski@keitaro.com>
2023-05-17 10:17:05 +0200

    Update home link

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,7 +7,7 @@ export const SideBar: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div data-testid="Sidebar" className={className}>
       <div className="bg-gray-light sticky-top shadow shadow-lg d-flex justify-content-center flex-column align-items-center py-5 px-3">
-        <a href="/image-generator" rel='bookmark'>
+        <a href="/blog-image-generator" rel='bookmark'>
           <img className="img-fluid sidebar-logo mb-2" src={keitaroLogo} alt="Keitaro Logo" />
         </a>
         <p className="mb-0 bg-white py-1 px-2 fs-6 fw-bold">
