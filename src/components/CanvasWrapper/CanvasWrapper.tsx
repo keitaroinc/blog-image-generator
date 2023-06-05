c3a53e4c8efcf604a797b0621ae2d5ddc8e6a667
Goce Mitevski <goce.mitevski@keitaro.com>
2023-06-05 13:14:40 +0200

    Decrease initial pixelRatio to 1

--- src/components/CanvasWrapper/CanvasWrapper.tsx
+++ src/components/CanvasWrapper/CanvasWrapper.tsx
@@ -47,7 +47,7 @@ export const CanvasWrapper: React.FC<{ className?: string }> = ({
         return;
       }
       if (type === "png") {
-        toPng(canvasRef.current, { cacheBust: true, pixelRatio: 2 })
+        toPng(canvasRef.current, { cacheBust: true, pixelRatio: 1 })
           .then((dataUrl) => {
             const link = document.createElement("a");
             link.download = `${canvasHeadlineValues.content
