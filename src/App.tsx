a52a507fe54c243de9734025ad37788a8b4c85ad
Goce Mitevski <goce.mitevski@keitaro.com>
2023-06-08 18:45:16 +0200

    Move CanvasPreviewContextProvider above row

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { CanvasPreviewContextProvider } from "./contexts/CanvasPreviewContext";
 
 export const App = () => {
   return (
-    <div className="row g-0 vh-100 overflow-hidden">
-      <CanvasPreviewContextProvider>
-        <SideBar className="col-md-3 d-flex flex-column bg-gray-light sidebar h-100 overflow-auto" />
-        <CanvasWrapper className="col-md-9 d-flex flex-column justify-content-center align-items-center canvas-wrapper" />
-      </CanvasPreviewContextProvider>
-    </div>
+    <CanvasPreviewContextProvider>
+      <div className="row g-0">
+        <SideBar className="col-xl-3 sidebar" />
+        <CanvasWrapper className="col-xl-9 canvas-wrapper" />
+      </div>
+    </CanvasPreviewContextProvider>
   );
 };
