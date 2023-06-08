4111655c5e56c125436ae73267c8820890438a3f
Goce Mitevski <goce.mitevski@keitaro.com>
2023-06-08 18:59:49 +0200

    Decrease default headline size

--- src/contexts/CanvasPreviewContext.tsx
+++ src/contexts/CanvasPreviewContext.tsx
@@ -36,7 +36,7 @@ export const Provider = (props: {
     useState<CanvasHeadlineValuesInterface>({
       content: "Headline text goes here and it can get pretty long",
       color: "#000000",
-      size: 4,
+      size: 3,
       position: {
         x: 2,
         y: 2,
