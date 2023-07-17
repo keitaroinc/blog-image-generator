ac5586db5340db331925f6663f95922528feaea0
Goce Mitevski <goce.mitevski@keitaro.com>
2023-07-17 09:59:33 +0200

    Add icon to Dialog component

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -4,18 +4,24 @@ interface DialogProps {
   children: any;
   id: string;
   title: string;
+  icon: string;
 }
 
 export const Dialog: React.FunctionComponent<DialogProps> = ({
   children,
   id,
   title,
+  icon,
 }) => {
   return (
-    <div className={`modal fade`} id={id} tabIndex={-1} aria-hidden="true">
+    <div className={`modal modal-lg fade`} id={id} tabIndex={-1} aria-hidden="true">
       <div className="modal-dialog modal-dialog-scrollable">
         <div className="modal-content">
           <div className="modal-header">
+            <i
+              className={`bi bi-${icon} me-2`}
+              title={`${title} icon`}
+            ></i>
             <h1 className="modal-title fs-5" id="exampleModalLabel">
               {title}
             </h1>
