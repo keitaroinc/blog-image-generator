2e1812daefe44edfd15e29d9e01abd269b41f1b2
darkoilievski <darko.ilievski@keitaro.com>
2024-12-19 15:03:28 +0100

    Fix npm run build, and fix the unit tests

--- src/components/__tests__/SideBar.test.tsx
+++ src/components/__tests__/SideBar.test.tsx
@@ -1,44 +1,44 @@
-// import { render, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// import { SideBar } from "../SideBar/SideBar";
-// import { CanvasPreviewContextProvider } from "../../contexts/CanvasPreviewContext";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { SideBar } from "../SideBar/SideBar";
+import { CanvasPreviewContextProvider } from "../../contexts/CanvasPreviewContext";
 
-// const MockSideBar = () => {
-//   return (
-//     <CanvasPreviewContextProvider>
-//       <SideBar />
-//     </CanvasPreviewContextProvider>
-//   );
-// };
+const MockSideBar = () => {
+  return (
+    <CanvasPreviewContextProvider>
+      <SideBar />
+    </CanvasPreviewContextProvider>
+  );
+};
 
-// describe("SideBar.tsx tests", () => {
-//   it("should render a component with test-id canvasHeadline", () => {
-//     render(<MockSideBar />);
-//     expect(screen.getByTestId("canvasHeadline")).toBeInTheDocument();
-//   });
+describe("SideBar.tsx tests", () => {
+  it("should render a component with test-id canvasHeadline", () => {
+    render(<MockSideBar />);
+    expect(screen.getByTestId("canvasHeadline")).toBeInTheDocument();
+  });
 
-//   it("should render a component with test-id canvasLogo", () => {
-//     render(<MockSideBar />);
-//     expect(screen.getByTestId("canvasLogo")).toBeInTheDocument();
-//   });
+  it("should render a component with test-id canvasLogo", () => {
+    render(<MockSideBar />);
+    expect(screen.getByTestId("canvasLogo")).toBeInTheDocument();
+  });
 
-//   it("should render a component with test-id canvasBackground", () => {
-//     render(<MockSideBar />);
-//     expect(screen.getByTestId("canvasBackground")).toBeInTheDocument();
-//   });
+  it("should render a component with test-id canvasBackground", () => {
+    render(<MockSideBar />);
+    expect(screen.getByTestId("canvasBackground")).toBeInTheDocument();
+  });
 
-//   it("should render a component with test-id canvasIcon", () => {
-//     render(<MockSideBar />);
-//     expect(screen.getByTestId("canvasIcon")).toBeInTheDocument();
-//   });
+  it("should render a component with test-id canvasIcon", () => {
+    render(<MockSideBar />);
+    expect(screen.getByTestId("canvasIcon")).toBeInTheDocument();
+  });
 
-//   it("should render a component with test-id canvasBorder", () => {
-//     render(<MockSideBar />);
-//     expect(screen.getByTestId("canvasBorder")).toBeInTheDocument();
-//   });
+  it("should render a component with test-id canvasBorder", () => {
+    render(<MockSideBar />);
+    expect(screen.getByTestId("canvasBorder")).toBeInTheDocument();
+  });
 
-//   it("should render an element with alt text Keitaro Logo", () => {
-//     render(<MockSideBar />);
-//     expect(screen.getByAltText("Keitaro Logo")).toBeInTheDocument();
-//   });
-// });
+  it("should render an element with alt text Keitaro Logo", () => {
+    render(<MockSideBar />);
+    expect(screen.getByAltText("Keitaro Logo")).toBeInTheDocument();
+  });
+});
