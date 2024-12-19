fe1bd12bd40ec985876b2525f5fc07e1485e2934
darkoilievski <darko.ilievski@keitaro.com>
2024-12-19 15:50:53 +0100

    Upgrade to react 19

--- src/components/__tests__/CanvasHeadline.test.tsx
+++ src/components/__tests__/CanvasHeadline.test.tsx
@@ -1,21 +1,21 @@
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// import { CanvasHeadline } from "../CanvasHeadline/CanvasHeadline";
-// import { CanvasPreviewContextProvider } from "../../contexts/CanvasPreviewContext";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { CanvasHeadline } from "../CanvasHeadline/CanvasHeadline";
+import { CanvasPreviewContextProvider } from "../../contexts/CanvasPreviewContext";
 
-// describe("CanvasHeadlines.tsx tests", () => {
-//   it("should be able to type into textarea input element", () => {
-//     render(
-//       <CanvasPreviewContextProvider>
-//         <CanvasHeadline />
-//       </CanvasPreviewContextProvider>
-//     );
-//     const textAreaElement = screen.getByPlaceholderText(
-//       /Enter headline text here/i
-//     ) as HTMLInputElement;
-//     fireEvent.change(textAreaElement, {
-//       target: { value: "Text for the headline component" },
-//     });
-//     expect(textAreaElement.value).toBe("Text for the headline component");
-//   });
-// });
+describe("CanvasHeadlines.tsx tests", () => {
+  it("should be able to type into textarea input element", () => {
+    render(
+      <CanvasPreviewContextProvider>
+        <CanvasHeadline />
+      </CanvasPreviewContextProvider>
+    );
+    const textAreaElement = screen.getByPlaceholderText(
+      /Enter headline text here/i
+    ) as HTMLInputElement;
+    fireEvent.change(textAreaElement, {
+      target: { value: "Text for the headline component" },
+    });
+    expect(textAreaElement.value).toBe("Text for the headline component");
+  });
+});
