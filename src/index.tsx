a6b0e57b1cb389fbec2680bc9ce2f3add26fb139
Goce Mitevski <goce.mitevski@gmail.com>
2025-03-12 18:16:52 +0100

    Implement ReactGA

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_GTAG);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
